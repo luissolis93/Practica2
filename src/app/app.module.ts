ae114bc2893a191ce96357dca1333d7f992e468d
luis solis <00269648luis@gmail.com>
2019-10-23 13:22:42 -0500

    practicas de Angular app practica 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import {HeaderComponent} from './components/header/header.component';
+import { SectionImagesComponent } from './components/section-images/section-images.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SectionImagesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/section-images/section-images.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section-images',
+  templateUrl: './section-images.component.html',
+  styles: []
+})
+export class SectionImagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
